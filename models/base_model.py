2ebaf7a45da31d3e460a964eeb422caa86222d40
EvaMicich <6366@holbertonstudents.com>
2023-07-04 00:13:55 -0700

    corrected file starge attributes, help from Jon

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-'''
+"""
 Module that creates the class Basemodel and all
 common attributes/methods for other classes
-'''
+"""
 
 import uuid
 from datetime import datetime
+from models.engine.file_storage import FileStorage
 
+storage = FileStorage
 
 class BaseModel:
-    '''
+    """
     Base class for other classes
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
-        '''
+        """
         Initialize the BaseModel instance.
-        '''
+        """
         if kwargs != {}:
             for key, value in kwargs.items():
                 if key == 'id':
@@ -31,14 +33,15 @@ class BaseModel:
                     self.updated_at = datetime.now().fromisoformat(value)
 
         else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+#            storage.new(self)
 
     def __str__(self):
-        '''
+        """
         Return a string representation of the instance.
-        '''
+        """
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
 
@@ -47,13 +50,14 @@ class BaseModel:
         Update the updated_at attribute with the current datetime.
         '''
         self.updated_at = datetime.now()
+#        storage.save()
 
     def to_dict(self):
         """
         Returns a dictionary containing all keys/values of the instance
         """
         new_dict = self.__dict__.copy()
-        new_dict["__class__"] = f"{self.__class__.__name__}"
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = new_dict["created_at"].isoformat()
         new_dict["updated_at"] = new_dict["updated_at"].isoformat()
         return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,32 +10,42 @@ import json
 class FileStorage():
     """
     Class called file storage
+
+    Arguments:
+    file_path: string - path to the JSON file (ex: file.json
+    objects: dictionary - empty but will store all objects by <class name>.id
     """
-    def __init__(self, file_path, objects):
+
+    def __init__(self, file_path="", objects={}):
+        """initial file storage"""
         self.__file_path = file_path
         self.__objects = objects
 
-    @property
     def all(self):
         """
         Get the obj dict
         """
         return self.__objects
 
-    @new.setter
     def new(self, obj):
         """
         Setting in new dict
         """
-        key loop thing here
-        self.__objects = value
+        self.__objects[f"{type(obj).__name__}.{obj.id}"] = obj
 
     def save(self):
         """
-        serializes json to file
+        serializes __objects to the JSON file
         """
+        with open(self.__file_path, 'w') as serial_obj:
+            json.dump(self.__objects, serial_obj)
 
     def reload(self):
         """
         deserializes json to file
         """
+        try:
+            with open(self.__file_path, encoding="utf-8") as obj_from_json:
+                return(json.load(obj_from_json))
+        except:
+            pass
