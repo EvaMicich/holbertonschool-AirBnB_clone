bb932d2007718e94fc2fbf9a8fd8d7effe65e482
BryceKnight16 <6373@holbertonstudents.com>
2023-07-03 18:02:51 -0700

    real base

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,48 @@
 #!/usr/bin/python3
-"""
-Base Class
-"""
-import uuid
-import datetime
+'''
+Module that creates the class Basemodel and all
+common attributes/methods for other classes
+'''
 
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """
-    BaseModel class
-    """
-    def __init__(self, id=None,
+    '''
+    Base class for other classes
+    '''
+
+    def __init__(self, name=None, my_number=None, id=None,
                  created_at=None, updated_at=None):
-
+        '''
+        Initialize the BaseModel instance.
+        '''
+        self.name = name
+        self.my_number = my_number
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Print str format: [<class name>] (<self.id>) <self.__dict__>
-        """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        '''
+        Return a string representation of the instance.
+        '''
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Updates the attribute updated_at with the current datetime
-        """
-        self.updated_at = datetime.datetime.now()
-
+        '''
+        Update the updated_at attribute with the current datetime.
+        '''
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__
+        Returns a dictionary containing all keys/values of the instance
         """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-        inst_dict["id"] = self.id
-        inst_dict["created_at"] = self.created_at.isoformat()
-
-        return inst_dict
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = f"{self.__class__.__name__}"
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        return new_dict
