ea7654fe57df0de2507b439c27f3c49cd4d6f63a
BryceKnight16 <6373@holbertonstudents.com>
2023-07-06 03:02:29 -0700

    unittest checker

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+"""
+Unittest for BaseModel()
+"""
+
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = BaseModel(
+            id='123',
+            name='test',
+            my_number=42,
+            created_at=datetime.now().isoformat(),
+            updated_at=datetime.now().isoformat()
+        )
+        self.string_representation = str(self.model)
+        self.initial_updated_at = self.model.updated_at
+        self.model_dict = self.model.to_dict()
+
+    def test_init_with_args(self):
+        self.assertEqual(self.model.id, '123')
+
+    def test_init_without_args(self):
+        self.assertIsInstance(self.model.id, str)
+
+    def test_init_created_at(self):
+        self.assertIsInstance(self.model.created_at, datetime)
+
+    def test_init_updated_at(self):
+        self.assertIsInstance(self.model.updated_at, datetime)
+
+    def test_str_contains_class_name(self):
+        self.assertIn('[BaseModel]', self.string_representation)
+
+    def test_str_contains_id(self):
+        self.assertIn(self.model.id, self.string_representation)
+
+    def test_save_updates_updated_at(self):
+        self.model.save()
+        self.assertNotEqual(self.initial_updated_at, self.model.updated_at)
+
+    def test_to_dict_returns_dict(self):
+        self.assertIsInstance(self.model_dict, dict)
+
+    def test_to_dict_contains_class_name(self):
+        self.assertEqual(self.model_dict['__class__'], 'BaseModel')
+
+    def test_to_dict_contains_id(self):
+        self.assertEqual(self.model_dict['id'], self.model.id)
+
+    def test_to_dict_contains_created_at(self):
+        self.assertEqual(self.model_dict['created_at'], self.model.created_at.isoformat())
+
+    def test_to_dict_contains_updated_at(self):
+        self.assertEqual(self.model_dict['updated_at'], self.model.updated_at.isoformat())
+
