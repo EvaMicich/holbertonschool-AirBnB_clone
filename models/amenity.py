db15a03c9a28a54cf247fb5076f11971a979291a
BryceKnight16 <6373@holbertonstudents.com>
2023-07-06 21:42:33 -0700

    adding extra classes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that contains class Amenitys inheriting BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module that has the class State in it
+"""
+
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    Class State
+    """
+    name = ""
