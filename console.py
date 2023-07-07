574c22dd0e75f77627b8e12d931c9325f7ba0a41
EvaMicich <6366@holbertonstudents.com>
2023-07-07 05:44:21 -0700

    added create method to consol

--- console.py
+++ console.py
@@ -3,11 +3,22 @@
 consol model, entry point for the consol
 """
 import cmd, sys
-
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """command line interpreter, entry point"""
     prompt = '(hbnb)'
+
+    def do_create(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            my_model = BaseModel()
+            my_model.save()
+            print(my_model.id)
 
     def do_quit(self, arg):
         'Quit command to exit the program'
